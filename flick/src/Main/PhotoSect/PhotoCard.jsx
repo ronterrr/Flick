630febfc898cc3e6e5060de929665025cf49fdbb
Daveny Kuria <davenykuria@gmail.com>
2026-08-09 17:41:30 +0300

    create PhotoCard component

--- flick/src/Main/PhotoSect/PhotoCard.jsx
+++ flick/src/Main/PhotoSect/PhotoCard.jsx
@@ -1,5 +1,13 @@
-function PhotoCard(){
-
+function PhotoCard({photo, caption, username}){
+    return(
+        <>
+        <div className="photo-card">
+            <img src={photo}/>
+            <p className="caption">{caption}</p>
+            <p className="username">{username}</p>
+        </div>
+        </>
+    )
 }
 
 export default PhotoCard;
