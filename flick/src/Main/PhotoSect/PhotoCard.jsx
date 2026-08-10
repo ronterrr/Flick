7a97e89a1beefe92935809ef958dd91002bf945a
Daveny Kuria <davenykuria@gmail.com>
2026-08-10 11:12:46 +0300

    integrate modal into photocard

--- flick/src/Main/PhotoSect/PhotoCard.jsx
+++ flick/src/Main/PhotoSect/PhotoCard.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+import Modal from "../Modals/Modal";
+
 function PhotoCard({ photo, caption, username }) {
+  const [isZoomed, setIsZoomed] = useState(false);
+
   return (
     <>
       <div className="photo-card">
-        <img src={photo} />
+        <img
+          src={photo}
+          onClick={() => {
+            setIsZoomed(true);
+          }}
+        />
         <p className="caption">{caption}</p>
         <p className="username">~ {username}</p>
       </div>
+
+      <Modal isOpen={isZoomed} onClose={()=>{setIsZoomed(false)}}>
+          <div className="zoomed-container">
+            <img src={photo} className="zoomed-image"/>
+            <p className="caption">{caption}</p>
+            <p className="username">~ {username}</p>
+          </div>
+      </Modal>
     </>
   );
 }
