6e10c82ef27692d31006968bbe244cbca2086b29
Daveny Kuria <davenykuria@gmail.com>
2026-08-10 17:25:33 +0300

    implement drag blocking for images globally

--- flick/src/main.jsx
+++ flick/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './PhotoFeed'
-import Login from './Login'
+import App from "./PhotoFeed";
+import Login from "./Login";
 
-createRoot(document.getElementById('root')).render(
+import "./Global/dragblock.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Login />
+    <App />
   </StrictMode>,
-)
+);
