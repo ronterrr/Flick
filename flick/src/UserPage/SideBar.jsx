3b623f6aafcd1c1ba94128c32856898fe401cfb2
Daveny Kuria <davenykuria@gmail.com>
2026-08-14 22:27:42 +0300

    added designing for sidebar

--- flick/src/UserPage/SideBar.jsx
+++ flick/src/UserPage/SideBar.jsx
@@ -1,17 +1,36 @@
-import './sidebar.css'
+import { useState } from "react";
+import "./sidebar.css";
 
 function SideBar() {
+
+    const [selectedOption, setSelectedOption] = useState('Account');
+
+  const menuItems = [
+    { name: "Account" },
+    { name: "Privacy & Safety" },
+    { name: "Security & Login" },
+    { name: "Notifications" },
+    { name: "Content & Display Preferences" },
+    { name: "Support & Legal" },
+  ];
+
   return (
     <>
       <div className="side-bar">
         <h3>Settings</h3>
         <hr />
-        <h4>Account</h4>
-        <h4>Privacy & Safety</h4>
-        <h4>Security & Login</h4>
-        <h4>Notifications</h4>
-        <h4>Content & Display Preferences</h4>
-        <h4>Support & Legal</h4>
+
+        {menuItems.map((i) => {
+          return <h4 
+          key={i.name}
+          onClick={() => {
+            setSelectedOption(i.name)
+          }}
+          className={selectedOption === i.name ? 'option-selected' : ''}>
+            {i.name}
+            </h4>;
+        })}
+
         <h4 className="signout-btn">SIGN OUT</h4>
       </div>
     </>
