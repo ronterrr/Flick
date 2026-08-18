1f41f245d65bff1639189c4295b8c5e1e524f441
Daveny Kuria <davenykuria@gmail.com>
2026-08-18 11:45:34 +0300

    add page header text and navbar for GenAPI

--- flick/src/GeneratorAPI/index.jsx
+++ flick/src/GeneratorAPI/index.jsx
@@ -1,5 +1,6 @@
 import Logo from "../Global/Header/Logo";
 import UserProfile from "../Global/Header/UserProfile";
+import NavBar from "../PhotoFeed/Navigation/NavBar";
 
 import "./index.css";
 
@@ -9,8 +10,10 @@ function GenAPI({ userIndex = 0 }) {
       <div className="gen-api">
         <div className="top-section">
           <Logo />
+          <h3 className="page-header">MUSIC GENRE GENERATOR</h3>
           <UserProfile userIndex={userIndex} />
         </div>
+        <NavBar/>
       </div>
     </>
   );
